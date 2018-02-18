761f8c3e5939f4c0b783a04eb6d89f9d24306c25
Boris Sedov <borissedov@gmail.com>
2018-02-18 14:05:40 +0300

    Componets folder rename

--- ToptalTimezones/ClientApp/routes.tsx
+++ ToptalTimezones/ClientApp/routes.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import { Route } from 'react-router-dom';
-import { Layout } from './components/Shared/Layout';
-import HomePage from './components/HomePage/HomePage';
-import FetchData from './components/FetchDataPage/FetchDataPage';
-import CounterPage from './components/CounterPage/CounterPage';
-import { PrivateRoute } from './components/Shared/PrivateRoute'
+import { Layout } from './_components/Shared/Layout';
+import HomePage from './_components/HomePage/HomePage';
+import FetchData from './_components/FetchDataPage/FetchDataPage';
+import CounterPage from './_components/CounterPage/CounterPage';
+import { PrivateRoute } from './_components/Shared/PrivateRoute'
 
-import LoginPage from './components/LoginPage/LoginPage';
-import RegisterPage from './components/RegisterPage/RegisterPage';
-import { LogoutComponent } from './components/Shared'
+import LoginPage from './_components/LoginPage/LoginPage';
+import RegisterPage from './_components/RegisterPage/RegisterPage';
+import { LogoutComponent } from './_components/Shared'
 
 export const routes = <Layout>
     <Route exact path='/' component={HomePage} />
